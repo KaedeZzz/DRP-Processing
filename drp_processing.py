cfc520998a91a846d60ce3ec54d4e9fd7c285ccc
Zhanfeng Zhou <zhanfengzhou0408@gmail.com>
2025-08-25 13:50:51 +0100

    refactor(DRP): refactored file name, put yaml read to global

--- drp_processing.py
+++ drp_processing.py
@@ -14,6 +14,7 @@ from tqdm import tqdm
 import numpy as np
 from PIL import Image
 from scipy.signal import wiener
+from matplotlib import pyplot as plt
 
 from utils import load_images
 
@@ -21,6 +22,29 @@ cwd = Path.cwd()
 
 with open("config.yaml", 'r') as stream:
     exp_param = yaml.safe_load(stream)
+
+# Assign parameters locally
+th_min = exp_param['th_min']
+th_max = exp_param['th_max']
+th_num = exp_param['th_num']
+ph_min = exp_param['ph_min']
+ph_max = exp_param['ph_max']
+ph_num = exp_param['ph_num']
+
+
+def build_angle_profile(num_images):
+    # Construct a profile of (phi, theta) angles for each image
+    if num_images != th_num * ph_num:
+        raise ValueError('The number of images loaded does not match angle range')
+    ph_th_profile = np.zeros((num_images, 2))
+    indexing = np.arange(1, num_images + 1)
+    phi_step = (ph_max - ph_min) / (ph_num - 1)
+    th_step = (th_max - th_min) / (th_num - 1)
+    for i in range(num_images):
+        ph_th_profile[i, 0] = ((indexing[i] - 1) // th_num) * phi_step
+        ph_th_profile[i, 1] = ((indexing[i] - 1) % th_num) * th_step + th_min
+    return ph_th_profile
+
 
 def drp_loader(folder='images', img_format='jpg', roi: list | np.array = None):
     """
@@ -31,14 +55,6 @@ def drp_loader(folder='images', img_format='jpg', roi: list | np.array = None):
     :param roi: The region of interest.
     :return: a stack of images, and a 2D array of elevation and azimuth angles of each image.
     """
-
-    # Assign parameters locally
-    th_min = exp_param['th_min']
-    th_max = exp_param['th_max']
-    th_num = exp_param['th_num']
-    ph_min = exp_param['ph_min']
-    ph_max = exp_param['ph_max']
-    ph_num = exp_param['ph_num']
 
     if len(roi) != 4:
         raise ValueError('Region of interest must contain 4 coordinates')
@@ -61,16 +77,7 @@ def drp_loader(folder='images', img_format='jpg', roi: list | np.array = None):
         if roi is not None:
             images[i] = images[i].crop(*roi)
 
-    # Construct a profile of (phi, theta) angles for each image
-    ph_th_profile = np.zeros((num_images, 2))
-    indexing = np.arange(1, num_images + 1)
-    phi_step = (ph_max - ph_min) / (ph_num - 1)
-    th_step = (th_max - th_min) / (th_num - 1)
-    for i in range(num_images):
-        ph_th_profile[i, 0] = ((indexing[i] - 1) // th_num) * phi_step
-        ph_th_profile[i, 1] = ((indexing[i] - 1) % th_num) * th_step + th_min
-
-    return images, ph_th_profile
+    return images, build_angle_profile(num_images)
 
 
 def bg_subtraction(samples: list[Image.Image], backgrounds: list[Image.Image], coeff: float = 1.0) -> list[Image.Image]:
