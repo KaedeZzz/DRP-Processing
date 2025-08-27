458843c42b8eeaaea1a0a2b792365bf39329389f
Zhanfeng Zhou <zhanfengzhou0408@gmail.com>
2025-08-27 11:28:39 +0100

    chore(DRP): changed sample image for DRP point selection

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from drp_processing import drp_loader, drp_measure
 if __name__ == "__main__":
     images, profile = drp_loader()
     # dataset = igrey2drp(images)
-    drp_measure(images[0], images=images)
+    drp_measure(img_sample=images[17], images=images) # pick image at a highest angle as sample image (to select location from)
