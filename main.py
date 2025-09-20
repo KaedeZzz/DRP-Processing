d0b3b7e4dcff21103d3f9cd5667206f9a42eb6af
KaedeZzz <zhanfengzhou0408@gmail.com>
2025-09-21 00:05:58 +0800

    chore(main): new ROI compatibility

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from drp_processing import drp_loader, drp_measure, area_drp
+from drp_processing import drp_loader, display_drp, area_mean_drp
 from direction import drp_direction_map, get_drp_direction
+from utils import ROI
 
 if __name__ == "__main__":
     images, profile = drp_loader()
     # dataset = igrey2drp(images)
-    roi = [100, 200, 100, 200]
-    drp_mat = area_drp(images, roi, display=True) # pick image at a highest angle as sample image (to select location from)
-    print(get_drp_direction(drp_mat))
+    roi = ROI(100, 90, 100, 100)
+    mean_drp_mat = area_mean_drp(images, roi, display=True) # display image selected at highest elevation angle
+    display_drp(mean_drp_mat)
+    print(get_drp_direction(mean_drp_mat))
     drp_direction_map(images, roi)
