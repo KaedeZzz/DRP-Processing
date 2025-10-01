86a9cfbf1f5b2840857c67df0b7ccad74125f393
KaedeZzz <zhanfengzhou0408@gmail.com>
2025-10-02 01:19:14 +0800

    feat(mask): added linear multiplication for gamma transform

--- utils/masks.py
+++ utils/masks.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def gamma_transform(img: np.ndarray, gamma: float = 2, epsilon: int = 5, return_type: str = 'float64')-> np.ndarray:
+def gamma_transform(img: np.ndarray, gamma: float = 2, epsilon: int = 5, linear_coeff: float = 2, return_type: str = 'float64')-> np.ndarray:
     """Perform gamma transform on an image. Return type can be 'float64' or 'uint8'."""
     if img.dtype == np.uint8:
         img += epsilon
@@ -12,6 +12,7 @@ def gamma_transform(img: np.ndarray, gamma: float = 2, epsilon: int = 5, return_
         raise TypeError('mask dtype must be either np.uint8 or np.float64')
 
     img = np.power(img, gamma)
+    img *= linear_coeff
 
     if return_type == 'float64':
         img = np.clip(img, 0, 1).astype(np.float64)
