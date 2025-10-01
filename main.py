959069a89f6a29e5bbdcccaf2fef8bff645a0ffd
KaedeZzz <zhanfengzhou0408@gmail.com>
2025-10-02 01:19:34 +0800

    feat(main): directional masking

--- main.py
+++ main.py
@@ -8,20 +8,12 @@ from matplotlib import pyplot as plt
 if __name__ == "__main__":
     images, profile = drp_loader()
     # dataset = igrey2drp(images)
-    roi = ROI(0, 0, 100, 50)
+    roi = ROI(0, 0, 3840, 2160)
     mean_drp_mat = area_mean_drp(images, roi, display=True) # display image selected at highest elevation angle
     display_drp(mean_drp_mat)
-    print(get_drp_direction(mean_drp_mat))
     mag_map, deg_map = drp_direction_map(images, roi)
-    img_mask = drp_mask_angle(mag_map, deg_map, orientation=0, threshold=30)
-    masked_images = mask_images(images, img_mask, roi=roi)
-    masked_arrays = [np.array(image) for image in masked_images]
-    for i in range(len(masked_arrays)):
-        if i % 5 == 0 and i <= 20:
-            plt.imshow(masked_arrays[i])
-    plt.show()
-    transformed_arrays = [gamma_transform(masked_array, return_type='uint8') for masked_array in masked_arrays]
-    transformed_images = [Image.fromarray(array) for array in transformed_arrays]
-    for i in range(len(transformed_images)):
-        if i % 5 == 0 and i <= 20:
-            transformed_images[i].show()
+    img_mask = drp_mask_angle(mag_map, deg_map, orientation=90, threshold=30)
+    masked_images = mask_images(images, img_mask, roi=roi, normalize=True)
+    for i in range(len(images)):
+        if i % 100 == 0:
+            masked_images[i].show()
